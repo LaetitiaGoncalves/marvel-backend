30ee132a199eb999d0dc6781c1c65190cc7b070e
Laetitia Goncalves <laetitiagoncalves@macbook-pro-de-laetitia.home>
2022-08-03 22:06:07 +0200

    resolve mistakes

--- server.js
+++ server.js
@@ -9,13 +9,14 @@ const axios = require("axios");
 app.use(express.json());
 //récupérer la liste des comics dans l'API MARVEL
 
-app.get("/", async (req, res) => {
+app.get("/comics", async (req, res) => {
   const apikey = process.env.API_KEY;
   const url = `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${apikey}`;
 
   try {
     const response = await axios.get(url);
     res.status(200).json(response.data);
+    console.log(response.data);
   } catch (error) {
     res.status(400).json({ message: "no" });
   }
