a2ab01140fe49a20c59afa8cab95100f53f59c80
Laetitia Goncalves <laetitiagoncalves@macbook-pro-de-laetitia.home>
2022-08-03 20:31:37 +0200

    resolving cors prob

--- server.js
+++ server.js
@@ -9,11 +9,6 @@ const axios = require("axios");
 app.use(express.json());
 //récupérer la liste des comics dans l'API MARVEL
 
-const corsOptions = {
-  origin: "https://laetitia-marvel-project.netlify.app/",
-  optionsSuccessStatus: 200,
-};
-
 app.get("/comics", cors(corsOptions), async (req, res) => {
   const apikey = process.env.API_KEY;
   const url = `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${apikey}`;
@@ -21,10 +16,6 @@ app.get("/comics", cors(corsOptions), async (req, res) => {
   try {
     const response = await axios.get(url);
     res.status(200).json(response.data);
-    res.header(
-      "Access-Control-Allow-Origin",
-      "https://laetitia-marvel-project.netlify.app/"
-    );
   } catch (error) {
     res.status(400).json({ message: "Page not found" });
   }
