9a5814e972a7cb6ece7c49d427b0fd67310fe142
Laetitia Goncalves <laetitiagoncalves@MacBook-Pro-de-Laetitia.local>
2022-08-04 14:01:38 +0200

    add character route

--- server.js
+++ server.js
@@ -9,15 +9,26 @@ app.use(cors());
 app.use(express.json());
 //récupérer la liste des comics dans l'API MARVEL
 
+const apikey = process.env.API_KEY;
+
 app.get("/", (req, res) => {
   res.status(500).json("welcome");
 });
 
 app.get("/comics", async (req, res) => {
   try {
-    const apikey = process.env.API_KEY;
     const url = `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${apikey}`;
     const response = await axios.get(url);
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: "error" });
+  }
+});
+
+app.get("/characters", async (req, res) => {
+  try {
+    const url = `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${apikey}`;
+    const response = await axios(url);
     res.status(200).json(response.data);
   } catch (error) {
     res.status(400).json({ message: "error" });
